4a2c444df775504a7257d33eb4bdfae406e11074
Shruti Gupta <shruti.gupta@nagarro.com>
2024-04-10 20:06:34 +0530

    Initial deployment of our empty pipeline

---
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
+
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkpipelinesDemoPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+        //new CodePipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
+
+       // How it will be built and synthesized
+       synth: new ShellStep('Synth', {
+         // Where the source can be found
+         input: CodePipelineSource.gitHub('shrutig348/repo2', 'main'),
+         
+         // Install dependencies, build and run cdk synth
+         commands: [
+           'npm ci',
+           'npm run build',
+           'npx cdk synth'
+         ],
+       }),
+    });
+
+    // This is where we add the application stages
+    // ...
+  }
+}
